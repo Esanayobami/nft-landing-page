3b5c300da70510baa8176de3170e59eb633d867e
Esan Ayobami <103492958+Esanayobami@users.noreply.github.com>
2023-03-26 16:19:18 -0400

    Update Hero_Data.js

--- src/Component/Home/Hero_Data.js
+++ src/Component/Home/Hero_Data.js
@@ -1,4 +1,5 @@
 export const HeroData = {
     heroDescription : " Discover , buy and sell rare collection of digital Arts and NFTs.",
     heroSubDescription : "Digital marketplace for crypto collectibles and non- fungible tokens NFTS" ,
+    Bluedes: "discover",
 }
