947aa23aa8f20606e0e1ff7b5a0b9442f08a9459
dicey <esanelijah@icloud.com>
2023-03-29 17:21:57 -0700

    new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Component/Header/HeaderComponent";
 import Hero from "./Component/Home/HeroComponent";
+import Auction from "./Component/Auction/AuctionComponent";
 function App() {
 
   return (
     <div className="px-3 md:px-8 xl:px-16">
         <Header />
         <Hero />
+        <Auction />
     </div>
   )
 }
